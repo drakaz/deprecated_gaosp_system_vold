cbcf3830c022bd847c395b2412b4651645979a34
drakaz <drakpub@gmail.com>
2011-03-09 22:44:55 +0100

    Fix a merge issue on vold
    Change-Id: I11b4933fb71021cec7f85a86517625eb0690e05b

--- VolumeManager.cpp
+++ VolumeManager.cpp
@@ -1109,22 +1109,15 @@ int VolumeManager::shareVolume(const char *label, const char *method) {
              sizeof(nodepath), "/dev/block/vold/%d:%d",
              MAJOR(d), MINOR(d));
 
-    // TODO: Currently only two mounts are supported, defaulting
-    // /mnt/sdcard to lun0 and anything else to lun1. Fix this.
-    if (0 == strcmp(label, "/mnt/sdcard")) {
-        if ((fd = open("/sys/devices/platform/usb_mass_storage/lun0/file",
-                       O_WRONLY)) < 0) {
-            SLOGE("Unable to open ums lunfile (%s)", strerror(errno));
-            return -1;
-        }
+	if ((fd = open("/sys/devices/platform/usb_mass_storage/lun0/file", O_WRONLY)) < 0) {
+		SLOGE("Unable to open ums lunfile (%s)", strerror(errno));
+        return -1;
     }
-    else {
-        if ((fd = open("/sys/devices/platform/usb_mass_storage/lun1/file",
-                       O_WRONLY)) < 0) {
-            SLOGE("Unable to open ums lunfile (%s)", strerror(errno));
-            return -1;
-        }
-    }
+    if (write(fd, nodepath, strlen(nodepath)) < 0) {
+        SLOGE("Unable to write to ums lunfile (%s)", strerror(errno));
+        close(fd);
+        return -1;
+    } 
     
     // drakaz : force external sdcard in ums mode if exist
     if (FILE * file = fopen("/dev/block/mmcblk1p1", "r")) {
